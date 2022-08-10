ad369a23f9115d4ff2fd9f76a726befdcbddf7c1
Alfredo Vilca <1923050415@untels.edu.pe>
2022-08-10 11:07:07 -0500

    CRUD Proveedor - Listo

--- miapp/models.py
+++ miapp/models.py
@@ -10,3 +10,13 @@ class Producto(models.Model):
     precioventa = models.TextField()
     cantidad = models.IntegerField()
     estado = models.TextField()
+
+
+class Provedor(models.Model):
+    idprovedor = models.CharField(primary_key=True,max_length=6)
+    razonsocial = models.TextField()
+    ruc = models.TextField()
+    direccion = models.TextField()
+    telefono= models.TextField()
+    representantelegal = models.TextField()
+    estado = models.TextField()
